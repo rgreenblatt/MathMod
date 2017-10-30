2dde615b8b398f442e59379f9ad44ef02b7f4147
Ryan Greenblatt <hobo.ryang@gmail.com>
2017-10-29 23:57:22 -0400

    Added iter_model and standardized function outputs. Classes and iter_model NEED to be cleaned up.

--- initial.py
+++ initial.py
@@ -20,10 +20,9 @@ pplant=np.full((grid,grid),0.0)
 pfish=np.full((grid,grid),1.0)
 posprey=np.full((grid,grid),1.0)
 
-def metabolism(energies,organism,dt,grid):
-    for i in range(grid):
-        for k in range(grid):
-            energies[i][k]-=energies[i][k]*organism.metrate*dt
+def metabolism(energies,organism,dt):
+    return -energies*organism.metrate*dt
+    
 
 '''
 def neighfind(i,k,grid):
@@ -51,8 +50,8 @@ def neighfind(i,k,grid):
 def diffusion(organism,energies,dt):
     kernel = [[1,1,1],[1,-8,1],[1,1,1]]
     diffused = np.multiply(energies,organism.diffusion*dt/8.)
-    energies = energies + signal.convolve2d(diffused,kernel,boundary='wrap',mode='same')
-    return energies
+    return signal.convolve2d(diffused,kernel,boundary='wrap',mode='same')
+    
 
 eosprey[0][0]=90
 
@@ -61,10 +60,40 @@ for time in range(20):
     eosprey=temp
     print(temp[0][0])
 print(osprey.predation)
-effeciency = .5
+efficiency = .5
 epredators = np.array([eosprey])
 for i in range(5):
-	efish, epredators = consume.consume(efish, epredators, np.array([[osprey.predation, osprey.maxdist, effeciency]]), grid, .1)
+	efish, epredators = consume.consume(efish, epredators, np.array([[osprey.predation, osprey.maxdist, efficiency]]), grid, .1)
 #print(efish)
 #print(eosprey)
+
+#below function is under heavy development, should change substantially
+def iter_model(organisms, energies, pollutions, plant_ambient, dt, grid, efficiency):
+	x, plantAdd = consume.consume(np.full((grid,grid), plant_ambient), np.array([energies[0]]), np.array([[plant.predation, plant.maxdist, efficiency]]), grid, dt)
+	
+	plantRem, fishAdd = consume.consume(energies[0], np.array([energies[1]]), np.array([[fish.predation, fish.maxdist, efficiency]]), grid, dt)
+
+	fishRem, ospreyAdd = consume.consume(energies[1], np.array([energies[2]]), np.array([[osprey.predation, ospprey.maxdist, efficiency]]), grid, dt)
+
+	diffusDeltas = np.array([])
+	for i in range(len(organisms)):
+		diffDeltas.append(diffusion(organisms[i], energies[i], dt))
+	
+	metDeltas = np.array([])
+	for i in range(len(organisms)):
+		metDeltas.append(metabolism(energies[i], organisms[i], dt))
+	
+	
+	
+	
+	energies[0] = energies[0] + plantAdd[0] + plantRem + diffusDeltas[0] + metDeltas[0]
+	energies[1] = energies[1] + fishAdd[0] + fishRem + diffusDeltas[1] + metDeltas[1]
+	energies[2] = energies[2] + ospreyAdd[0] + diffusDeltas[2] + metDeltas[2]
+
+	#TODO POLLUTION
+	return energies#, pollutions
+
+
+
+	
 
