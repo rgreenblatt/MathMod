aab479b899b87edfaa9f933be495c885342abb03
Karna Morey <Karna_Morey@Karnas-MacBook-Pro.local>
2017-10-31 19:55:13 -0400

    added energy addition

--- generateScenario.py
+++ generateScenario.py
@@ -3,20 +3,21 @@ import classes
 import random
 import graphics
 import numpy as np
-def start():
+def start(energy):
     out=[]
     total=np.zeros((100,100))
     for i in range(10):
         out.append((random.randrange(100),random.randrange(100)))
     for point in out:
-        total[point[0]][point[1]]=100
+        total[point[0]][point[1]]=energy
     for i in range(3):
         total=total+initial.diffusion(.9,total,1)
         #print(total)
     return total
 
-def setup(orgs):
-    listy=[start() for i in orgs]
+def setup(energies):
+    
+    listy=[start(element) for element in energies]
     return tuple(listy)
 
 #hello=setup()
---
+++ pollutant.py
@@ -0,0 +1,8 @@
+def logistic(energy,
+
+
+def pollutApply(energy,pollution,chemindex,organism):
+    ld50=tox[chemical.index][organism.index]
+    for i in range(energy.shape[0]):
+        
+    
