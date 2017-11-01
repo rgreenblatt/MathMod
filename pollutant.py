a048648b4df88981484e575690b67758bb569c29
Karna Morey <Karna_Morey@Karnas-MacBook-Pro.local>
2017-10-31 23:18:44 -0400

    committed the pollutant function

--- pollutant.py
+++ pollutant.py
@@ -1,8 +1,18 @@
-def logistic(energy,
+import math
+import graphics
+import numpy as np
+
+def logistic(center,conc,rate=2):
+    return 1/(1+math.e**(-rate*(conc-center)))
 
 
-def pollutApply(energy,pollution,chemindex,organism):
-    ld50=tox[chemical.index][organism.index]
+def pollutApply(energy,pollution,ld50):
+    out=np.zeros(energy.shape)
     for i in range(energy.shape[0]):
-        
-    
+        for k in range(energy.shape[1]):
+            change=logistic(ld50,pollution[i][k],rate=5*ld50)
+            if i==1 and k==1:
+                print(change)
+            out[i][k]=-change*energy[i][k]
+    return out
+
